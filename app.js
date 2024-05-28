60e07037590bc022cbfb3b5e85d73133a87152a2
Ivan Herrera Barrios <ivanherrerabarrios16@gmail.com>
2024-05-27 20:49:39 -0500

    482. License Key Formatting

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var licenseKeyFormatting = function(s, k) {
+    let str = s.replace(/\-/g, "").toUpperCase()
+
+    let ret = ""
+    while(str.length > 0) {
+        const group = str.substring(str.length - k)
+        ret = "-" + group + ret
+        str = str.substring(0, str.length - k)
+    }
+    return ret[0] === "-" ? ret.slice(1) : ret
+};
